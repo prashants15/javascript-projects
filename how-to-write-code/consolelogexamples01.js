80384dd4f8a3b084e5d123c6a877c64071872c5b
prashants15 <134793634+prashants15@users.noreply.github.com>
2023-08-17 23:21:39 -0500

    file 1 committing

--- how-to-write-code/consolelogexamples01.js
+++ how-to-write-code/consolelogexamples01.js
@@ -5,3 +5,4 @@ console.log("Does", "adding",      "space", "matter?");
 console.log('Launch' + 'Code');
 console.log("LaunchCode was founded in", 2013);
 console.log("Welcome to \n Javascript");
+//
